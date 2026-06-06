073b373f2c7a76dc9a0b76b6f0f5b5f76d8761ce
Musdalifahthul Jennah <144792540+musdalifafaifa@users.noreply.github.com>
2026-06-06 18:45:47 +0700

    Fix(Sidebar) : access role petugas SIK

--- app/Views/admin/layout.php
+++ app/Views/admin/layout.php
@@ -98,7 +98,7 @@
                 $navMain[] = ['key' => 'rules', 'label' => 'Rule Klasifikasi', 'href' => site_url('admin/rule-klasifikasi'), 'icon' => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2'];
             }
 
-            if (in_array($role, ['petugas poli', 'admin', 'superadmin'], true)) {
+            if (in_array($role, ['petugas sik', 'admin', 'superadmin'], true)) {
                 $navMain[] = ['key' => 'laporan', 'label' => 'Laporan', 'href' => site_url('admin/laporan'), 'icon' => 'M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'];
             }
 
